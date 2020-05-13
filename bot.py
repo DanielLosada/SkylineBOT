825f19b51e05656dda2f67d4c4bca7ea561041e8
dani <suportinformatic@esplac.cat>
2020-05-13 19:16:09 +0200

    sadasd

--- bot.py
+++ bot.py
@@ -1,7 +1,47 @@
+# importa l'API de Telegram
+import telegram
+from telegram.ext import Updater, Filters
+from telegram.ext import Updater, CommandHandler
+
+# defineix una funció que saluda i que s'executarà quan el bot rebi el missatge /start
+def start(update, context):
+    botname = context.bot.username
+    username = update.effective_chat.first_name
+    missatge = "Hola %s, soc en %s" % (username, botname)
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=missatge
+        
+    )
+    
+def help(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Soc un bot amb comandes /start i /help.")
+
+# declara una constant amb el access token que llegeix de token.txt
+TOKEN = open('token.txt').read().strip()
+
+# crea objectes per treballar amb Telegram
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# indica que quan el bot rebi la comanda /start s'executi la funció start
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help))
+
+
+# engega el bot
+updater.start_polling()
+
+
+'''
 import telegram
 from telegram.ext import Updater, Filters
 from telegram.ext import CommandHandler, MessageHandler
 
+#t.me/DanLMBOT
 
 def start(bot, update): #(bot,callbackquery)
     botname = bot.username
@@ -11,12 +51,20 @@ def start(bot, update): #(bot,callbackquery)
     print("Start command")
 
 
-TOKEN = open('token.txt').read().strip()
+def help(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Soc un bot amb comandes /start, /help i /hora.")
 
+
+
+TOKEN = open('token.txt').read().strip()
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help))
+
 #dispatcher.add_handler(CommandHandler('help', help))
 #dispatcher.add_handler(CommandHandler('author', author))
 #dispatcher.add_handler(CommandHandler('lst', lst))
@@ -30,3 +78,4 @@ dispatcher.add_handler(CommandHandler('start', start))
 #        ))
 
 updater.start_polling()
+'''
