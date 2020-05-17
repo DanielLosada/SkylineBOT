5a2ea6d15e8d2d12b50dfe0ea97eb353aba3f119
dani <suportinformatic@esplac.cat>
2020-05-17 19:03:30 +0200

    calculaPosReflec i reflectit

--- skyline.py
+++ skyline.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 class Skyline:
     nom = "1"
-    a = [(1,2,3), (3,4,6)]
+    a = [(1,2,2), (2,4,6)]
 
     def generarFigura(self):
         
@@ -33,6 +33,36 @@ class Skyline:
         plt.bar(pos,height,width=width)
         plt.show()
         '''
+        
+    def calculPosReflex(self,min,max,pos):
+        llargada = max-min
+        puntMitg = min + (llargada/2)
+        distPMitgMax = puntMitg - min
+        if pos < puntMitg: 
+            distPMitg = puntMitg - pos
+            ret = pos + distPMitg*2
+            return ret
+        elif pos > puntMitg:
+            distPMitg = pos - puntMitg
+            ret = pos - distPMitg*2
+            return ret
+        else:
+            return pos
+
+    
+
+
+    def reflectit(self):
+        min, max = [self.a[0][0], self.a[-1][-1]]
+        ret = []
+        for x in self.a:
+            print("x[0]: ", x[0])
+            print("x[1]: ", x[1])
+            print("x[2]: ", x[2])
+            aux = (self.calculPosReflex(min,max,x[2]),x[1],self.calculPosReflex(min,max,x[0]))
+            ret = ret + [aux]            
+        print("ret: ", ret)
+        self.a = ret
 
     def introduce_self(self):
         print("My name is " + self.nom)
