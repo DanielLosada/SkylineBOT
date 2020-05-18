578e2e76552d47584f45779267117311770e1e6a
dani <suportinformatic@esplac.cat>
2020-05-18 19:40:21 +0200

    takeFirst, ordenaPlots, fic Reflexa, multiplicaSkyline

--- skyline.py
+++ skyline.py
@@ -1,33 +1,43 @@
 import matplotlib.pyplot as plt
 
+def takeFirst(elem):
+    return elem[0]
+
 class Skyline:
     nom = "1"
-    a = [(1,2,2), (2,4,6)]
+    plots = [(1,2,3), (3,4,6)]
+
+   
+
+    def ordenaPlots(self):
+        self.plots.sort(key=takeFirst)
+        
 
     def generarFigura(self):
         
-        height = [x[1] for x in self.a]
-        width = [x[2]-x[0] for x in self.a]
-        pos = [((x[2]-x[0])/2)+x[0] for x in self.a]
+        height = [x[1] for x in self.plots]
+        width = [x[2]-x[0] for x in self.plots]
+        pos = [((x[2]-x[0])/2)+x[0] for x in self.plots]
+        plt.clf()
         plt.bar(pos,height,width=width) 
         plt.savefig("Skyline_" + self.nom)
         #plt.show()
 
     def unio(self, a):
-        height = [x[1] for x in self.a] + [x[1] for x in a]
-        width = [x[2]-x[0] for x in self.a] + [x[2]-x[0] for x in a]
-        pos = [((x[2]-x[0])/2)+x[0] for x in self.a] + [((x[2]-x[0])/2)+x[0] for x in a]
+        height = [x[1] for x in self.plots] + [x[1] for x in a]
+        width = [x[2]-x[0] for x in self.plots] + [x[2]-x[0] for x in a]
+        pos = [((x[2]-x[0])/2)+x[0] for x in self.plots] + [((x[2]-x[0])/2)+x[0] for x in a]
         plt.bar(pos,height,width=width)
         plt.show()
         
         '''
-        height1 = [x[1] for x in self.a]
+        height1 = [x[1] for x in self.plots]
         height2 = [x[1] for x in a]
         height = height1 + height2
-        width1 = [x[2]-x[0] for x in self.a]
+        width1 = [x[2]-x[0] for x in self.plots]
         width2 = [x[2]-x[0] for x in a]
         width = width1 + width2
-        pos1 = [((x[2]-x[0])/2)+x[0] for x in self.a]
+        pos1 = [((x[2]-x[0])/2)+x[0] for x in self.plots]
         pos2 = [((x[2]-x[0])/2)+x[0] for x in a]
         pos = pos1 + pos2
         plt.bar(pos,height,width=width)
@@ -37,7 +47,6 @@ class Skyline:
     def calculPosReflex(self,min,max,pos):
         llargada = max-min
         puntMitg = min + (llargada/2)
-        distPMitgMax = puntMitg - min
         if pos < puntMitg: 
             distPMitg = puntMitg - pos
             ret = pos + distPMitg*2
@@ -50,19 +59,21 @@ class Skyline:
             return pos
 
     
-
+    
 
     def reflectit(self):
-        min, max = [self.a[0][0], self.a[-1][-1]]
+        min, max = [self.plots[0][0], self.plots[-1][-1]]
         ret = []
-        for x in self.a:
-            print("x[0]: ", x[0])
-            print("x[1]: ", x[1])
-            print("x[2]: ", x[2])
-            aux = (self.calculPosReflex(min,max,x[2]),x[1],self.calculPosReflex(min,max,x[0]))
-            ret = ret + [aux]            
-        print("ret: ", ret)
-        self.a = ret
+        for x in self.plots:
+            ret = ret + [(self.calculPosReflex(min,max,x[2]),x[1],self.calculPosReflex(min,max,x[0]))]            
+        self.plots = ret
+        self.ordenaPlots()
 
-    def introduce_self(self):
-        print("My name is " + self.nom)
+    def multiplicaSkyline(self, num):
+        amplada = self.plots[-1][-1] - self.plots[0][0]
+        ret = []
+        for x in range(1,num):
+            for y in self.plots:
+                ret = ret + [(y[0]+(amplada*x), y[1],y[2]+(amplada*x))]
+        self.plots = self.plots + ret
+        
